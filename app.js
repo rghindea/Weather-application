c8d4b3157d7489d7ef539b443b80baeef26b5c74
Raluca Ghindea <ralucaghindea@yahoo.com>
2024-07-28 21:45:22 +0300

    feat: show weather - fetch api

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const showWeatherBtn = document.getElementById("show-weather");
 const cityInput = document.getElementById("city");
-
+const weatherContainer = document.getElementById("weather-container");
 showWeatherBtn.addEventListener("click", showWeather);
+
+const URL_CURRENT_WEATHER =
+  "https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=";
 
 async function showWeather() {
   const city = cityInput.value;
-  console.log(city);
+  const response = await fetch(`${URL_CURRENT_WEATHER}${city}`);
+  const weather = await response.json();
+  console.log(weather);
+
+  const iconCode = weather.weather[0].icon;
+  const iconImageUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+
+  weatherContainer.innerHTML = `
+  <div>
+<img src = ${iconImageUrl} />
+<p> Descriere: ${weather.weather[0].description} </p>
+<p> Umiditate: ${weather.main.humidity} </p>
+<p> Presiune: ${weather.main.pressure} </p>
+<p> Temperatura curenta: ${weather.main.temp} </p>
+<p>Maxima zilei: ${weather.main.temp_max} </p>
+         <p>Minima zilei: ${weather.main.temp_min} </p>
+
+
+  </div>
+  `;
 }
